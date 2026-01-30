51c670d4c21cd1cf36b80f345f1129679fced3ed
Anton <90362671+abarbarich@users.noreply.github.com>
2026-01-30 21:48:22 +1300

    Added Attributes as a fixed option

--- src/common/attributes/useCommonDeviceAttributes.js
+++ src/common/attributes/useCommonDeviceAttributes.js
@@ -1,6 +1,7 @@
 import { useMemo } from 'react';
 
 export default (t) => useMemo(() => ({
+  // --- EXISTING STANDARD ATTRIBUTES ---
   speedLimit: {
     name: t('attributeSpeedLimit'),
     type: 'number',
@@ -30,4 +31,39 @@ export default (t) => useMemo(() => ({
     name: t('attributeNotificationTokens'),
     type: 'string',
   },
+
+  // --- NEW UI CONTROL ATTRIBUTES ---
+  enableImmobiliser: {
+    name: 'UI: Enable Immobiliser Mode',
+    type: 'boolean',
+  },
+  enableOutput: {
+    name: 'UI: Enable Generic Output Mode',
+    type: 'boolean',
+  },
+  
+  // --- NEW UI INPUT CONFIGURATION ---
+  input1Type: {
+    name: 'UI: Input 1 Type (bilge, fuel, battery, door)',
+    type: 'string',
+  },
+  input1Source: {
+    name: 'UI: Input 1 Data Source (default: in1)',
+    type: 'string',
+  },
+  input2Type: {
+    name: 'UI: Input 2 Type (bilge, fuel, battery, door)',
+    type: 'string',
+  },
+  input2Source: {
+    name: 'UI: Input 2 Data Source (default: in2)',
+    type: 'string',
+  },
+  
+  // --- NEW UI FUEL CONFIGURATION ---
+  fuelSource: {
+    name: 'UI: Fuel Data Source (default: fuelLevel)',
+    type: 'string',
+  },
+
 }), [t]);
